556c5c1768734b1b2ce2d5588570432b330f37e8
Bedo <bedonassef71@gmail.com>
2023-06-15 12:43:45 +0300

    Table Names & Primary Keys & Incrementing False & Timestamps False

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // if table in db name not posts
+    protected $table = "my_posts";
+
+    // if  table don't have timestamps
+    public $timestamps = false;
+
+    // if PK name not id
+    protected $primaryKey = "post_id";
+
+    // not auto increment
+    public $incrementing = false;
+
+    // if u don't use mysql (use mongoose)
+    protected $connection='sqlite';
 }
