fbfa6c9578d33cdee9439a9de6e5113650dda967
Bedo <bedonassef71@gmail.com>
2023-06-20 15:13:13 +0300

    create tables and put fake data

--- resources/views/posts/create.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Create new Post</h1>
-
-{{--<form action="{{url('posts/store')}}">--}}
-<form action="{{route('posts.store')}}" method="post">
-    @csrf
-    <input type="text" name="title" placeholder="enter title">
-    <input type="text" name="body" placeholder="enter body">
-    <button type="submit">Create</button>
-</form>
--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::group([
-    'prefix' => 'posts',
-    'namespace' => '\App\Http\Controllers',
-], function () {
-    Route::get('create', 'PostController@create');
-    Route::post('store', 'PostController@store')->name('posts.store');
-    Route::get('/', 'PostController@index')->name('posts.index')->middleware('check_user');
-    Route::get('{id}/edit', 'PostController@edit')->name('posts.edit');
-    Route::put('{id}/update', 'PostController@update')->name('posts.update');
-    Route::delete('{id}/delete', 'PostController@destroy')->name('posts.destroy');
-    Route::get('/archive', 'PostController@archive')->name('posts.archive');
-    Route::post('/{id}/restore', 'PostController@restore')->name('posts.restore');
-    Route::delete('/{id}/forceDelete', 'PostController@forceDelete')->name('posts.forceDelete');
-});
+Route::resource('countries', \App\Http\Controllers\CountryController::class);
