fc5e9b88584df5c273bb0886a0b8892e773d5aa9
Bedo <bedonassef71@gmail.com>
2023-06-20 14:34:31 +0300

    php artisan make:command MakeViewCommand

---
+++ app/Console/Commands/MakeViewCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeViewCommand extends Command
+{
+    protected $signature = 'make-view';
+
+    protected $description = 'create new blade file';
+
+    public function handle()
+    {
+        //
+    }
+}
