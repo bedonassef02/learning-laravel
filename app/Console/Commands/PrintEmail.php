b36affc612607c576ad62fdb87ccdfff2feeb12a
Bedo <bedonassef71@gmail.com>
2023-06-20 14:23:46 +0300

    php artisan print-email after this type ur name

--- app/Console/Commands/PrintEmail.php
+++ app/Console/Commands/PrintEmail.php
@@ -7,13 +7,13 @@ use Illuminate\Console\Command;
 class PrintEmail extends Command
 {
     // u can delete ?
-    protected $signature = 'print-email {email} {phone}';
+    protected $signature = 'print-email';
 
     protected $description = 'Print my Email';
 
     public function handle()
     {
-        $data = $this->arguments();
-        $this->info('print data ' . json_encode($data));
+        $name = $this->ask('what is ur name? ');
+        $this->info('print name ' . $name);
     }
 }
